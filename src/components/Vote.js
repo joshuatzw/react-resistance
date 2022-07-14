cc3f6ad9c663184804310febaef0fcc82a45f79c
annpng <joshua.tzw@gmail.com>
2022-07-14 17:36:46 +0800

    minor change to Vote compponent

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -4,19 +4,12 @@ import { ResourcesContext } from "../store/ResourcesContext"
 export default function Vote() {
   const resources = useContext(ResourcesContext)
 
-  function voteYes() {
-    resources.voteYes()
-  }
-
-  function voteNo() {
-    resources.voteNo()
-  }
-
   return(
     <div>
       <h1> {resources.currentCrewList[resources.votingIndex]} </h1>
       <button onClick={resources.voteYes}> Yes </button>
-      <button onClick={voteNo}> No </button> 
+      <button onClick={resources.voteNo}> No </button> 
     </div>
   )
+  // Some comments
 }
